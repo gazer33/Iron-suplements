d48ac06635c6240abf1383db8888cbd4b4c9d6fd
gazer33 <sebastiartt@gmail.com>
2025-05-27 21:19:49 -0600

    map

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,9 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 integrations: [tailwind()],
+ vite: {
+    define: {
+      'import.meta.env.GOOGLE_MAPS_KEY': JSON.stringify(process.env.GOOGLE_MAPS_KEY)
+    }
+  }
 });
